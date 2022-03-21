fdf8b784794bc2b8a2a344dfde5c9b442692ec0b
shoaibdevelop <shoaib.develops@outlook.com>
2022-03-21 22:04:48 +0500

    Initial Commit

---
+++ config.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    'secret': 'qwertyuiop',
+    //'database': 'mongodb://localhost/SocietyApp',
+    'database': 'mongodb+srv://TradierAdmin:Tradier123@tradiercluster.im6ox.mongodb.net/TradierDB?retryWrites=true&w=majority',
+    'salt' : 'DontTrust'
+};
